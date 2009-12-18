1b69ff1b76bad803be4a7d0ee782586b7b8d6ac1
Jean Boussier <jean.boussier@feedback20.com>
2009-12-18 23:13:56 +0100

    feature: base of GET and POST actions

--- server/chatserver.py
+++ server/chatserver.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
+from datetime import datetime
+import socket
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer, SocketServer
 
@@ -11,14 +14,36 @@ class PushyChatServer(SocketServer.ThreadingMixIn, HTTPServer):
 
 
 class PushyChatRequestHandler(SimpleHTTPRequestHandler):
+    messages = []
+    
+    def log_connection_close(self, error): # TODO: nice log
+        print 'connection closed by client'
     
     def do_GET(self):
         if not self.path.startswith('/chat/'):
             return SimpleHTTPRequestHandler.do_GET(self)
         
+        #self.last_check = self.headers[]
+        self.send_response(200)
+        self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        try:
+            while True:
+                self.wfile.write(self.find_new_messages())
+                time.sleep(1)
+        except socket.error, e:
+            self.log_connection_close(e)
+
+    def find_new_messages(self):
+        return 'foobar\n'
+    
     def do_POST(self):
-        print self._parse_params()
+        params =  self._parse_params()
+        self.send_new_message(params['login'], params['chan'], params['message'])
         self._redirect_to_root()
+
+    def send_new_message(self, login, chan, message):
+        self.messages.append({'login': login, 'chan': chan, 'message': message, 'time': time.time()})
 
     def _redirect_to_root(self):
         self.send_response(301)
